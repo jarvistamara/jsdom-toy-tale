468cf3092e426243715899a8dd9d3c616a746328
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-03-30 19:30:00 -0400

    commit tree 6fd69e4f3d899224a97a76f79ebc00bbaaf193da

--- src/index.js
+++ src/index.js
@@ -12,4 +12,72 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  fetchToys();
 });
+
+function fetchToys() {
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(renderToys)
+}
+
+let toyCollection = document.getElementById('toy-collection')
+function renderToys(toys) {
+  toyCollection.innerHTML = ""
+  toys.forEach(function (toy) {
+    toyCollection.innerHTML += `
+   <div class="card" data-id=${toy.id}>
+        <h2>${toy.name}</h2>
+        <img src="${toy.image}" class="toy-avatar" />
+        <p>${toy.likes} Likes</p>
+        <button class="like-btn">Like <3</button>
+        <button class="delete-btn">Delete</button>
+   </div>
+  `
+  })
+}
+
+const addToyForm = document.querySelector('.add-toy-form')
+addToyForm.addEventListener('submit', function (event) {
+  fetch(`http://localhost:3000/toys/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: `${event.target.name.value}`,
+      image: `${event.target.image.value}`,
+      likes: 0
+    })
+  })
+    .then(resp => resp.json())
+    .then(renderToys)
+})
+
+toyCollection.addEventListener('click', function (event) {
+  let likeButtonIsPressed = event.target.className === "like-btn"
+  let delButtonIsPressed = event.target.className === "delete-btn"
+  if (likeButtonIsPressed) {
+    let id = event.target.parentElement.dataset.id
+    let like = event.target.previousElementSibling
+    let likeCount = parseInt(event.target.previousElementSibling.innerText)
+    like.innerText = `${++likeCount} likes`
+fetch(`http://localhost:3000/toys/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        likes: likeCount
+      })
+    })
+    .then(response => response.json())
+  } else if (delButtonIsPressed) {
+    let id = event.target.parentElement.dataset.id
+    fetch(`http://localhost:3000/toys/${id}`, {
+      method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(fetchToys)
+  }
+})
